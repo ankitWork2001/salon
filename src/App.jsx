7e9c3565d87b997cefddacc50916db718fa79df8
Brajesh <kumarbrajesh8055@gmail.com>
2024-12-12 11:03:27 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import Navbar from './components/Navbar'
+import Top from './components/Top'
+import LandingPage from './Pages/LandingPage'
+
+function App() {
+
+  return (
+    <>
+      <Top/>
+      <Navbar/>
+      <LandingPage/>
+    </>
+  )
+}
+
+export default App
